ad2376cbdb99d28243eece9b9ab92e5f3d9928f8
SathtikBose <sathtikbose@gmail.com>
2024-11-14 18:18:41 +0530

    Update message

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.zomatowork.zomato',
-  appName: 'Zomato',
-  webDir: 'build'
-};
-
-export default config;
